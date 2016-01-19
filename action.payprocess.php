d53283aff03a3c7f4e83bbbdf6aa2cc8d4fe3a8a
tom2tom <tpgww@onepost.net>
2016-01-20 07:50:51 +1100

    cleanup

--- action.payprocess.php
+++ action.payprocess.php
@@ -14,53 +14,55 @@
  'stg_token' => stripe token
 */
 
+//clear all page-content echoed before now
+$handlers = ob_list_handlers();
+if($handlers)
+{
+	$l = count($handlers);
+	for ($c = 0; $c < $l; $c++)
+		ob_end_clean();
+}
+
 try {
 	require_once (dirname(__FILE__).DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'init.php');
-	$msg = '';	//default to 'successful-completion-signal'
 } catch (Exception $e) {
-	$msg = $this->Lang('err_system'); //TODO
+	die($this->Lang('err_system'));
 }
 
-if(!$msg)
+$row = $db->GetRow('SELECT currency,usetest,privtoken,testprivtoken FROM '.
+	cms_db_prefix().'module_sgt_account WHERE account_id=?',array($params['stg_account']));
+if($row['usetest'])
 {
-	$pref = cms_db_prefix();
-	$row = $db->GetRow('SELECT currency,usetest,privtoken,testprivtoken FROM '.
-	$pref.'module_sgt_account WHERE account_id=?',array($params['stg_account']));
-	if($row['usetest'])
-	{
-		if($row['testprivtoken'])
-			$privkey = sgtUtils::decrypt_value($this,$row['testprivtoken']);
-		else
-			$privkey = FALSE;
-	}
+	if($row['testprivtoken'])
+		$privkey = sgtUtils::decrypt_value($this,$row['testprivtoken']);
 	else
-	{
-		if($row['privtoken'])
-			$privkey = sgtUtils::decrypt_value($this,$row['privtoken']);
-		else
-			$privkey = FALSE;
-	}
-	if(!$privkey)
-	{
-		$msg = $this->Lang('err_parameter');
-	}
+		$privkey = FALSE;
+}
+else
+{
+	if($row['privtoken'])
+		$privkey = sgtUtils::decrypt_value($this,$row['privtoken']);
+	else
+		$privkey = FALSE;
+}
+if(!$privkey)
+{
+	die($this->Lang('err_parameter'));
 }
 
-if(!$msg)
-{
-	$data = array(
-		'amount' => $params['stg_amount'],
-		'currency' => $row['currency'],
-		'description' => 'prescribed payment',
-		'source' => $params['stg_token']
-	);
+$data = array(
+	'amount' => $params['stg_amount'],
+	'currency' => $row['currency'],
+	'description' => 'prescribed payment',
+	'source' => $params['stg_token']
+);
 
-	try
-	{
-		Stripe::setApiKey($privkey);
-		$charge = Stripe_Charge::create($data);
-		$response = $charge->__toArray(TRUE);
-		$sql = 'INSERT INTO '.$pref.'module_sgt_record (
+try
+{
+	Stripe::setApiKey($privkey);
+	$charge = Stripe_Charge::create($data);
+	$response = $charge->__toArray(TRUE);
+	$sql = 'INSERT INTO '.$pref.'module_sgt_record (
 account_id,
 amount,
 paywhat,
@@ -68,29 +70,19 @@ payfor,
 recorded,
 identifier
 ) VALUES(?,?,?,?,?,?)';
-		$db->Execute($sql,array(
-			$params['stg_account'],
-			$params['stg_amount'],
-			'prescribed amount',
-			'anonymous',
-			$response['created'],
-			$response['id']));
-	}
-	catch (Exception $e)
-	{
-		$msg = $e->getMessage();
-	}
-}
+	$db->Execute($sql,array(
+		$params['stg_account'],
+		$params['stg_amount'],
+		'prescribed amount',
+		$this->Lang('anonymous'),
+		$response['created'],
+		$response['id']));
 
-//clear all page content echoed before now
-$handlers = ob_list_handlers();
-if($handlers)
+	die(); //no message = success
+}
+catch (Exception $e)
 {
-	$l = count($handlers);
-	for ($cnt = 0; $cnt < $l; $cnt++)
-		ob_end_clean();
+	die($e->getMessage());
 }
-
-die($msg);
 
 ?>
