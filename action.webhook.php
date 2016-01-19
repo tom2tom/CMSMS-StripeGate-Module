ebcd8b38a6bb6e55513f2fc62378a4fdebb3b3df
tom2tom <tpgww@onepost.net>
2016-01-19 19:14:29 +1100

    skeleton action for webhook processing

---
+++ action.webhook.php
@@ -0,0 +1,23 @@
+<?php
+#----------------------------------------------------------------------
+# This file is part of CMS Made Simple module: StripeGate
+# Copyright (C) 2016 Tom Phane <tpgww@onepost.net>
+# Refer to licence and other details at the top of file StripeGate.module.php
+# More info at http://dev.cmsmadesimple.org/projects/stripegate
+#----------------------------------------------------------------------
+# This action handles webhook-reports from upstream Stripe
+
+//TODO process & record stuff
+
+//clear all page content echoed before now
+$handlers = ob_list_handlers();
+if($handlers)
+{
+	$l = count($handlers);
+	for ($cnt = 0; $cnt < $l; $cnt++)
+		ob_end_clean();
+}
+
+die(NOT YET SUPPORTED);
+
+?>
