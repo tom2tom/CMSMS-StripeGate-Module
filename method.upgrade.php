9cadc9e9f6c9e3ba0a52f9510e0b57b26ba03b1f
tom2tom <tpgww@onepost.net>
2017-04-10 10:25:22 +1000

    Conform revised directories layout Apply default masterpassword

--- method.upgrade.php
+++ method.upgrade.php
@@ -6,6 +6,21 @@
 # More info at http://dev.cmsmadesimple.org/projects/stripegate
 #----------------------------------------------------------------------
 
+function rmdir_recursive($dir)
+{
+	foreach(scandir($dir) as $file) {
+		if (!($file === '.' || $file === '..')) {
+			$fp = $dir.DIRECTORY_SEPARATOR.$file;
+			if (is_dir($fp)) {
+				rmdir_recursive($fp);
+			} else {
+ 				@unlink($fp);
+			}
+		}
+	}
+	rmdir($dir);
+}
+
 /*
 $db = cmsms()->GetDb();
 $dict = NewDataDictionary($db);
@@ -20,6 +35,8 @@ switch ($oldversion) {
 		unlink($fp);
 	}
  case '0.8.1':
+ case '0.9.0':
+	//redundant files
 	$base = __DIR__.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'class.';
 	foreach (['sgtUtils','sgtPayer','sgtExport'] as $fn) {
 		$fp = $base.$fn.'.php';
@@ -27,13 +44,21 @@ switch ($oldversion) {
 			unlink($fp);
 		}
 	}
- case '0.9.0':
+	//redundant directory
+	$fp = __DIR__.DIRECTORY_SEPARATOR.'include';
+	if (is_dir($fp)) {
+		rmdir_recursive($fp);
+	}
+
 	$t = 'nQCeESKBr99A';
 	$this->SetPreference($t, hash('sha256', $t.microtime()));
 	$pw = $this->GetPreference('masterpass');
 	if ($pw) {
 		$s = base64_decode(substr($pw,5));
 		$pw = substr($s,5);
+	}
+	if (!$pw) {
+		$pw = base64_decode('RW50ZXIgYXQgeW91ciBvd24gcmlzayEgRGFuZ2Vyb3VzIGRhdGEh');
 	}
 	$cfuncs = new StripeGate\Crypter($this);
 	$cfuncs->encrypt_preference('masterpass',$pw);
