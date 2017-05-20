31812d001917ab4154403523207ea671a01908a9
tom2tom <tpgww@onepost.net>
2017-05-20 20:06:19 +1000

    bump copyright year

--- action.deleterecord.php
+++ action.deleterecord.php
@@ -1,7 +1,7 @@
 <?php
 #----------------------------------------------------------------------
 # This file is part of CMS Made Simple module: StripeGate
-# Copyright (C) 2016 Tom Phane <tpgww@onepost.net>
+# Copyright (C) 2016-2017 Tom Phane <tpgww@onepost.net>
 # Refer to licence and other details at the top of file StripeGate.module.php
 # More info at http://dev.cmsmadesimple.org/projects/stripegate
 #----------------------------------------------------------------------
