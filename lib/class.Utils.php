beba4901cce188a074ca68fdc4573e2ab90621f2
tom2tom <tpgww@onepost.net>
2017-05-20 19:57:51 +1000

    import GetReportAmount() method

--- lib/class.Utils.php
+++ lib/class.Utils.php
@@ -88,7 +88,7 @@ class Utils
 	@cache: optional boolean, default TRUE
 	Returns: string, processed template
 	*/
-	public static function ProcessTemplate(&$mod, $tplname, $tplvars, $cache=TRUE)
+	public static function ProcessTemplate(&$mod, $tplname, $tplvars, $cache = TRUE)
 	{
 		if ($mod->before20) {
 			global $smarty;
@@ -106,12 +106,12 @@ class Utils
 				$cache_id = md5('sgt'.$tplname.serialize(array_keys($tplvars)));
 				$lang = \CmsNlsOperations::get_current_language();
 				$compile_id = md5('sgt'.$tplname.$lang);
-				$tpl = $smarty->CreateTemplate($mod->GetFileResource($tplname),$cache_id,$compile_id,$smarty);
+				$tpl = $smarty->CreateTemplate($mod->GetFileResource($tplname), $cache_id, $compile_id, $smarty);
 				if (!$tpl->isCached()) {
 					$tpl->assign($tplvars);
 				}
 			} else {
-				$tpl = $smarty->CreateTemplate($mod->GetFileResource($tplname),NULL,NULL,$smarty,$tplvars);
+				$tpl = $smarty->CreateTemplate($mod->GetFileResource($tplname), NULL, NULL, $smarty, $tplvars);
 			}
 			return $tpl->fetch();
 		}
@@ -128,8 +128,9 @@ class Utils
 		$ids = $db->GetArray(
 'SELECT account_id,isdefault FROM '.$pref.'module_sgt_account ORDER BY isdefault DESC,account_id');
 		if ($ids) {
-			if ($ids[0]['isdefault'] == TRUE || count($ids) == 1)
+			if ($ids[0]['isdefault'] == TRUE || count($ids) == 1) {
 				return (int)$ids[0]['account_id'];
+			}
 		}
 		return FALSE;
 	}
@@ -145,10 +146,12 @@ class Utils
 		$up = $config['uploads_path'];
 		if ($up) {
 			$rp = $mod->GetPreference('uploads_dir');
-			if ($rp)
+			if ($rp) {
 				$up .= DIRECTORY_SEPARATOR.$rp;
-			if (is_dir($up))
+			}
+			if (is_dir($up)) {
 				return $up;
+			}
 		}
 		return FALSE;
 	}
@@ -161,12 +164,12 @@ class Utils
 	public static function GetUploadsUrl(&$mod)
 	{
 		$config = \cmsms()->GetConfig();
-		$key = (empty($_SERVER['HTTPS'])) ? 'uploads_url':'ssl_uploads_url';
+		$key = (empty($_SERVER['HTTPS'])) ? 'uploads_url' : 'ssl_uploads_url';
 		$up = $config[$key];
 		if ($up) {
 			$rp = $mod->GetPreference('uploads_dir');
 			if ($rp) {
-				$rp = str_replace('\\','/',$rp);
+				$rp = str_replace('\\', '/', $rp);
 				$up .= '/'.$rp;
 			}
 			return $up;
@@ -197,34 +200,34 @@ class Utils
 			$all = [];
 		}
 		if ($jsfuncs || $jsloads) {
-			$all[] =<<<EOS
+			$all[] = <<<EOS
 <script type="text/javascript">
 //<![CDATA[
 EOS;
 			if (is_array($jsfuncs)) {
-				$all = array_merge($all,$jsfuncs);
+				$all = array_merge($all, $jsfuncs);
 			} elseif ($jsfuncs) {
 				$all[] = $jsfuncs;
 			}
 			if ($jsloads) {
-				$all[] =<<<EOS
+				$all[] = <<<EOS
 $(document).ready(function() {
 EOS;
 				if (is_array($jsloads)) {
-					$all = array_merge($all,$jsloads);
+					$all = array_merge($all, $jsloads);
 				} else {
 					$all[] = $jsloads;
 				}
-				$all[] =<<<EOS
+				$all[] = <<<EOS
 });
 EOS;
 			}
-			$all[] =<<<EOS
+			$all[] = <<<EOS
 //]]>
 </script>
 EOS;
 		}
-		$merged = implode(PHP_EOL,$all);
+		$merged = implode(PHP_EOL, $all);
 	}
 
 	/**
@@ -235,13 +238,14 @@ EOS;
 	*/
 	public static function ConstructAlias($alias, $fullname)
 	{
-		$alias = mb_convert_case(trim($alias),MB_CASE_LOWER); //TODO encoding
-		if (!$alias)
-			$alias = mb_convert_case(trim($fullname,"\t\n\r\0 _"),MB_CASE_LOWER);
-		$alias = preg_replace('/\W+/','_',$alias); //TODO mb_
-		$parts = array_slice(explode('_',$alias),0,5);
-		$alias = substr(implode('_',$parts),0,15);
-		return trim($alias,'_');
+		$alias = mb_convert_case(trim($alias), MB_CASE_LOWER); //TODO encoding
+		if (!$alias) {
+			$alias = mb_convert_case(trim($fullname, "\t\n\r\0 _"), MB_CASE_LOWER);
+		}
+		$alias = preg_replace('/\W+/', '_', $alias); //TODO mb_
+		$parts = array_slice(explode('_', $alias), 0, 5);
+		$alias = substr(implode('_', $parts), 0, 15);
+		return trim($alias, '_');
 	}
 
 	/**
@@ -251,141 +255,141 @@ EOS;
 	public static function GetSupportedCurrencies()
 	{
 		return [
-	'Afghan Afghani'=>'afn',
-	'Albanian Lek'=>'all',
-	'Algerian Dinar'=>'dzd',
-	'Angolan Kwanza'=>'aoa',
-	'Argentine Peso'=>'ars',
-	'Armenian Dram'=>'amd',
-	'Aruban Florin'=>'awg',
-	'Australian Dollar'=>'aud',
-	'Azerbaijani Manat'=>'azn',
-	'Bahamian Dollar'=>'bsd',
-	'Bangladeshi Taka'=>'bdt',
-	'Barbadian Dollar'=>'bbd',
-	'Belize Dollar'=>'bzd',
-	'Bermudian Dollar'=>'bmd',
-	'Bolivian Boliviano'=>'bob',
-	'Bosnia &amp; Herzegovina Convertible Mark'=>'bam',
-	'Botswana Pula'=>'bwp',
-	'Brazilian Real'=>'brl',
-	'British Pound'=>'gbp',
-	'Brunei Dollar'=>'bnd',
-	'Bulgarian Lev'=>'bgn',
-	'Burundian Franc'=>'bif',
-	'Cambodian Riel'=>'khr',
-	'Canadian Dollar'=>'cad',
-	'Cape Verdean Escudo'=>'cve',
-	'Cayman Islands Dollar'=>'kyd',
-	'Central African CFA Franc'=>'xaf',
-	'Cfp Franc'=>'xpf',
-	'Chilean Peso'=>'clp',
-	'Chinese Renminbi Yuan'=>'cny',
-	'Colombian Peso'=>'cop',
-	'Comorian Franc'=>'kmf',
-	'Congolese Franc'=>'cdf',
-	'Costa Rican Colón'=>'crc',
-	'Croatian Kuna'=>'hrk',
-	'Czech Koruna'=>'czk',
-	'Danish Krone'=>'dkk',
-	'Djiboutian Franc'=>'djf',
-	'Dominican Peso'=>'dop',
-	'East Caribbean Dollar'=>'xcd',
-	'Egyptian Pound'=>'egp',
-	'Ethiopian Birr'=>'etb',
-	'Euro'=>'eur',
-	'Falkland Islands Pound'=>'fkp',
-	'Fijian Dollar'=>'fjd',
-	'Gambian Dalasi'=>'gmd',
-	'Georgian Lari'=>'gel',
-	'Gibraltar Pound'=>'gip',
-	'Guatemalan Quetzal'=>'gtq',
-	'Guinean Franc'=>'gnf',
-	'Guyanese Dollar'=>'gyd',
-	'Haitian Gourde'=>'htg',
-	'Honduran Lempira'=>'hnl',
-	'Hong Kong Dollar'=>'hkd',
-	'Hungarian Forint'=>'huf',
-	'Icelandic Króna'=>'isk',
-	'Indian Rupee'=>'inr',
-	'Indonesian Rupiah'=>'idr',
-	'Israeli New Sheqel'=>'ils',
-	'Jamaican Dollar'=>'jmd',
-	'Japanese Yen'=>'jpy',
-	'Kazakhstani Tenge'=>'kzt',
-	'Kenyan Shilling'=>'kes',
-	'Kyrgyzstani Som'=>'kgs',
-	'Lao Kip'=>'lak',
-	'Lebanese Pound'=>'lbp',
-	'Lesotho Loti'=>'lsl',
-	'Liberian Dollar'=>'lrd',
-	'Macanese Pataca'=>'mop',
-	'Macedonian Denar'=>'mkd',
-	'Malagasy Ariary'=>'mga',
-	'Malawian Kwacha'=>'mwk',
-	'Malaysian Ringgit'=>'myr',
-	'Maldivian Rufiyaa'=>'mvr',
-	'Mauritanian Ouguiya'=>'mro',
-	'Mauritian Rupee'=>'mur',
-	'Mexican Peso'=>'mxn',
-	'Moldovan Leu'=>'mdl',
-	'Mongolian Tögrög'=>'mnt',
-	'Moroccan Dirham'=>'mad',
-	'Mozambican Metical'=>'mzn',
-	'Namibian Dollar'=>'nad',
-	'Nepalese Rupee'=>'npr',
-	'Netherlands Antillean Gulden'=>'ang',
-	'New Taiwan Dollar'=>'twd',
-	'New Zealand Dollar'=>'nzd',
-	'Nicaraguan Córdoba'=>'nio',
-	'Nigerian Naira'=>'ngn',
-	'Norwegian Krone'=>'nok',
-	'Pakistani Rupee'=>'pkr',
-	'Panamanian Balboa'=>'pab',
-	'Papua New Guinean Kina'=>'pgk',
-	'Paraguayan Guaraní'=>'pyg',
-	'Peruvian Nuevo Sol'=>'pen',
-	'Philippine Peso'=>'php',
-	'Polish Złoty'=>'pln',
-	'Qatari Riyal'=>'qar',
-	'Romanian Leu'=>'ron',
-	'Russian Ruble'=>'rub',
-	'Rwandan Franc'=>'rwf',
-	'Saint Helenian Pound'=>'shp',
-	'Salvadoran Colón'=>'svc',
-	'Samoan Tala'=>'wst',
-	'São Tomé &amp; Príncipe Dobra'=>'std',
-	'Saudi Riyal'=>'sar',
-	'Serbian Dinar'=>'rsd',
-	'Seychellois Rupee'=>'scr',
-	'Sierra Leonean Leone'=>'sll',
-	'Singapore Dollar'=>'sgd',
-	'Solomon Islands Dollar'=>'sbd',
-	'Somali Shilling'=>'sos',
-	'South African Rand'=>'zar',
-	'South Korean Won'=>'krw',
-	'Sri Lankan Rupee'=>'lkr',
-	'Surinamese Dollar'=>'srd',
-	'Swazi Lilangeni'=>'szl',
-	'Swedish Krona'=>'sek',
-	'Swiss Franc'=>'chf',
-	'Tajikistani Somoni'=>'tjs',
-	'Tanzanian Shilling'=>'tzs',
-	'Thai Baht'=>'thb',
-	'Tongan Paʻanga'=>'top',
-	'Trinidad &amp; Tobago Dollar'=>'ttd',
-	'Turkish Lira'=>'try',
-	'Ugandan Shilling'=>'ugx',
-	'Ukrainian Hryvnia'=>'uah',
-	'United Arab Emirates Dirham'=>'aed',
-	'United States Dollar'=>'usd',
-	'Uruguayan Peso'=>'uyu',
-	'Uzbekistani Som'=>'uzs',
-	'Vanuatu Vatu'=>'vuv',
-	'Vietnamese Đồng'=>'vnd',
-	'West African CFA Franc'=>'xof',
-	'Yemeni Rial'=>'yer',
-	'Zambian Kwacha'=>'zmw'
+	'Afghan Afghani' => 'afn',
+	'Albanian Lek' => 'all',
+	'Algerian Dinar' => 'dzd',
+	'Angolan Kwanza' => 'aoa',
+	'Argentine Peso' => 'ars',
+	'Armenian Dram' => 'amd',
+	'Aruban Florin' => 'awg',
+	'Australian Dollar' => 'aud',
+	'Azerbaijani Manat' => 'azn',
+	'Bahamian Dollar' => 'bsd',
+	'Bangladeshi Taka' => 'bdt',
+	'Barbadian Dollar' => 'bbd',
+	'Belize Dollar' => 'bzd',
+	'Bermudian Dollar' => 'bmd',
+	'Bolivian Boliviano' => 'bob',
+	'Bosnia &amp; Herzegovina Convertible Mark' => 'bam',
+	'Botswana Pula' => 'bwp',
+	'Brazilian Real' => 'brl',
+	'British Pound' => 'gbp',
+	'Brunei Dollar' => 'bnd',
+	'Bulgarian Lev' => 'bgn',
+	'Burundian Franc' => 'bif',
+	'Cambodian Riel' => 'khr',
+	'Canadian Dollar' => 'cad',
+	'Cape Verdean Escudo' => 'cve',
+	'Cayman Islands Dollar' => 'kyd',
+	'Central African CFA Franc' => 'xaf',
+	'Cfp Franc' => 'xpf',
+	'Chilean Peso' => 'clp',
+	'Chinese Renminbi Yuan' => 'cny',
+	'Colombian Peso' => 'cop',
+	'Comorian Franc' => 'kmf',
+	'Congolese Franc' => 'cdf',
+	'Costa Rican Colón' => 'crc',
+	'Croatian Kuna' => 'hrk',
+	'Czech Koruna' => 'czk',
+	'Danish Krone' => 'dkk',
+	'Djiboutian Franc' => 'djf',
+	'Dominican Peso' => 'dop',
+	'East Caribbean Dollar' => 'xcd',
+	'Egyptian Pound' => 'egp',
+	'Ethiopian Birr' => 'etb',
+	'Euro' => 'eur',
+	'Falkland Islands Pound' => 'fkp',
+	'Fijian Dollar' => 'fjd',
+	'Gambian Dalasi' => 'gmd',
+	'Georgian Lari' => 'gel',
+	'Gibraltar Pound' => 'gip',
+	'Guatemalan Quetzal' => 'gtq',
+	'Guinean Franc' => 'gnf',
+	'Guyanese Dollar' => 'gyd',
+	'Haitian Gourde' => 'htg',
+	'Honduran Lempira' => 'hnl',
+	'Hong Kong Dollar' => 'hkd',
+	'Hungarian Forint' => 'huf',
+	'Icelandic Króna' => 'isk',
+	'Indian Rupee' => 'inr',
+	'Indonesian Rupiah' => 'idr',
+	'Israeli New Sheqel' => 'ils',
+	'Jamaican Dollar' => 'jmd',
+	'Japanese Yen' => 'jpy',
+	'Kazakhstani Tenge' => 'kzt',
+	'Kenyan Shilling' => 'kes',
+	'Kyrgyzstani Som' => 'kgs',
+	'Lao Kip' => 'lak',
+	'Lebanese Pound' => 'lbp',
+	'Lesotho Loti' => 'lsl',
+	'Liberian Dollar' => 'lrd',
+	'Macanese Pataca' => 'mop',
+	'Macedonian Denar' => 'mkd',
+	'Malagasy Ariary' => 'mga',
+	'Malawian Kwacha' => 'mwk',
+	'Malaysian Ringgit' => 'myr',
+	'Maldivian Rufiyaa' => 'mvr',
+	'Mauritanian Ouguiya' => 'mro',
+	'Mauritian Rupee' => 'mur',
+	'Mexican Peso' => 'mxn',
+	'Moldovan Leu' => 'mdl',
+	'Mongolian Tögrög' => 'mnt',
+	'Moroccan Dirham' => 'mad',
+	'Mozambican Metical' => 'mzn',
+	'Namibian Dollar' => 'nad',
+	'Nepalese Rupee' => 'npr',
+	'Netherlands Antillean Gulden' => 'ang',
+	'New Taiwan Dollar' => 'twd',
+	'New Zealand Dollar' => 'nzd',
+	'Nicaraguan Córdoba' => 'nio',
+	'Nigerian Naira' => 'ngn',
+	'Norwegian Krone' => 'nok',
+	'Pakistani Rupee' => 'pkr',
+	'Panamanian Balboa' => 'pab',
+	'Papua New Guinean Kina' => 'pgk',
+	'Paraguayan Guaraní' => 'pyg',
+	'Peruvian Nuevo Sol' => 'pen',
+	'Philippine Peso' => 'php',
+	'Polish Złoty' => 'pln',
+	'Qatari Riyal' => 'qar',
+	'Romanian Leu' => 'ron',
+	'Russian Ruble' => 'rub',
+	'Rwandan Franc' => 'rwf',
+	'Saint Helenian Pound' => 'shp',
+	'Salvadoran Colón' => 'svc',
+	'Samoan Tala' => 'wst',
+	'São Tomé &amp; Príncipe Dobra' => 'std',
+	'Saudi Riyal' => 'sar',
+	'Serbian Dinar' => 'rsd',
+	'Seychellois Rupee' => 'scr',
+	'Sierra Leonean Leone' => 'sll',
+	'Singapore Dollar' => 'sgd',
+	'Solomon Islands Dollar' => 'sbd',
+	'Somali Shilling' => 'sos',
+	'South African Rand' => 'zar',
+	'South Korean Won' => 'krw',
+	'Sri Lankan Rupee' => 'lkr',
+	'Surinamese Dollar' => 'srd',
+	'Swazi Lilangeni' => 'szl',
+	'Swedish Krona' => 'sek',
+	'Swiss Franc' => 'chf',
+	'Tajikistani Somoni' => 'tjs',
+	'Tanzanian Shilling' => 'tzs',
+	'Thai Baht' => 'thb',
+	'Tongan Paʻanga' => 'top',
+	'Trinidad &amp; Tobago Dollar' => 'ttd',
+	'Turkish Lira' => 'try',
+	'Ugandan Shilling' => 'ugx',
+	'Ukrainian Hryvnia' => 'uah',
+	'United Arab Emirates Dirham' => 'aed',
+	'United States Dollar' => 'usd',
+	'Uruguayan Peso' => 'uyu',
+	'Uzbekistani Som' => 'uzs',
+	'Vanuatu Vatu' => 'vuv',
+	'Vietnamese Đồng' => 'vnd',
+	'West African CFA Franc' => 'xof',
+	'Yemeni Rial' => 'yer',
+	'Zambian Kwacha' => 'zmw'
 		];
 	}
 
@@ -397,146 +401,175 @@ EOS;
 	public static function GetSymbol($code)
 	{
 		$symbols = [
-		'afn'=>'؋',
-		'all'=>'Lek',
-		'dzd'=>'د.ج',
-		'aoa'=>'Kz',
-		'ars'=>'$',
-		'amd'=>'֏',
-		'awg'=>'ƒ',
-		'aud'=>'$',
-		'azn'=>'ман',
-		'bsd'=>'$',
-		'bdt'=>'৳',
-		'bbd'=>'$',
-		'bzd'=>'BZ$',
-		'bmd'=>'$',
-		'bob'=>'$b',
-		'bam'=>'KM',
-		'bwp'=>'P',
-		'brl'=>'R$',
-		'gbp'=>'£',
-		'bnd'=>'$',
-		'bgn'=>'лв',
-		'bif'=>'FBu',
-		'khr'=>'៛',
-		'cad'=>'$',
-		'cve'=>'$',
-		'kyd'=>'$',
-		'xaf'=>'FCFA',
-		'xpf'=>'F',
-		'clp'=>'$',
-		'cny'=>'¥',
-		'cop'=>'$',
-		'kmf'=>'CF',
-		'cdf'=>'FC',
-		'crc'=>'₡',
-		'hrk'=>'kn',
-		'czk'=>'Kč',
-		'dkk'=>'kr',
-		'djf'=>'Fdj',
-		'dop'=>'RD$',
-		'xcd'=>'$',
-		'egp'=>'£',
-		'etb'=>'Br',
-		'eur'=>'€',
-		'fkp'=>'£',
-		'fjd'=>'$',
-		'gmd'=>'D',
-		'gel'=>'₾',
-		'gip'=>'£',
-		'gtq'=>'Q',
-		'gnf'=>'FG',
-		'gyd'=>'$',
-		'htg'=>'G',
-		'hnl'=>'L',
-		'hkd'=>'$',
-		'huf'=>'Ft',
-		'isk'=>'kr',
-		'inr'=>'₹',
-		'idr'=>'Rp',
-		'ils'=>'₪',
-		'jmd'=>'J$',
-		'jpy'=>'¥',
-		'kzt'=>'лв',
-		'kes'=>'KSh',
-		'kgs'=>'лв',
-		'lak'=>'₭',
-		'lbp'=>'£',
-		'lsl'=>'L',
-		'lrd'=>'$',
-		'mop'=>'MOP$',
-		'mkd'=>'ден',
-		'mga'=>'Ar',
-		'mwk'=>'MK',
-		'myr'=>'RM',
-		'mvr'=>'Rf',
-		'mro'=>'UM',
-		'mur'=>'₨',
-		'mxn'=>'$',
-		'mdl'=>'L',
-		'mnt'=>'₮',
-		'mad'=>'.د.م',
-		'mzn'=>'MT',
-		'nad'=>'$',
-		'npr'=>'₨',
-		'ang'=>'ƒ',
-		'twd'=>'NT$',
-		'nzd'=>'$',
-		'nio'=>'C$',
-		'ngn'=>'₦',
-		'nok'=>'kr',
-		'pkr'=>'₨',
-		'pab'=>'B/.',
-		'pgk'=>'K',
-		'pyg'=>'Gs',
-		'pen'=>'S/.',
-		'php'=>'₱',
-		'pln'=>'zł',
-		'qar'=>'﷼',
-		'ron'=>'lei',
-		'rub'=>'₽',
-		'rwf'=>'FRw',
-		'shp'=>'£',
-		'svc'=>'$',
-		'wst'=>'WS$',
-		'std'=>'Db',
-		'sar'=>'﷼',
-		'rsd'=>'Дин.',
-		'scr'=>'₨',
-		'sll'=>'Le',
-		'sgd'=>'$',
-		'sbd'=>'$',
-		'sos'=>'S',
-		'zar'=>'R',
-		'krw'=>'₩',
-		'lkr'=>'₨',
-		'srd'=>'$',
-		'szl'=>'L',
-		'sek'=>'kr',
-		'chf'=>'Fr.',
-		'tjs'=>'',
-		'tzs'=>'TSh',
-		'thb'=>'฿',
-		'top'=>'T$',
-		'ttd'=>'TT$',
-		'try'=>'₺',
-		'ugx'=>'USh',
-		'uah'=>'₴',
-		'aed'=>'د.إ',
-		'usd'=>'$',
-		'uyu'=>'$U',
-		'uzs'=>'лв',
-		'vuv'=>'Vt.',
-		'vnd'=>'₫',
-		'xof'=>'CFA',
-		'yer'=>'﷼',
-		'zmw'=>'ZK'
+		'afn' => '؋',
+		'all' => 'Lek',
+		'dzd' => 'د.ج',
+		'aoa' => 'Kz',
+		'ars' => '$',
+		'amd' => '֏',
+		'awg' => 'ƒ',
+		'aud' => '$',
+		'azn' => 'ман',
+		'bsd' => '$',
+		'bdt' => '৳',
+		'bbd' => '$',
+		'bzd' => 'BZ$',
+		'bmd' => '$',
+		'bob' => '$b',
+		'bam' => 'KM',
+		'bwp' => 'P',
+		'brl' => 'R$',
+		'gbp' => '£',
+		'bnd' => '$',
+		'bgn' => 'лв',
+		'bif' => 'FBu',
+		'khr' => '៛',
+		'cad' => '$',
+		'cve' => '$',
+		'kyd' => '$',
+		'xaf' => 'FCFA',
+		'xpf' => 'F',
+		'clp' => '$',
+		'cny' => '¥',
+		'cop' => '$',
+		'kmf' => 'CF',
+		'cdf' => 'FC',
+		'crc' => '₡',
+		'hrk' => 'kn',
+		'czk' => 'Kč',
+		'dkk' => 'kr',
+		'djf' => 'Fdj',
+		'dop' => 'RD$',
+		'xcd' => '$',
+		'egp' => '£',
+		'etb' => 'Br',
+		'eur' => '€',
+		'fkp' => '£',
+		'fjd' => '$',
+		'gmd' => 'D',
+		'gel' => '₾',
+		'gip' => '£',
+		'gtq' => 'Q',
+		'gnf' => 'FG',
+		'gyd' => '$',
+		'htg' => 'G',
+		'hnl' => 'L',
+		'hkd' => '$',
+		'huf' => 'Ft',
+		'isk' => 'kr',
+		'inr' => '₹',
+		'idr' => 'Rp',
+		'ils' => '₪',
+		'jmd' => 'J$',
+		'jpy' => '¥',
+		'kzt' => 'лв',
+		'kes' => 'KSh',
+		'kgs' => 'лв',
+		'lak' => '₭',
+		'lbp' => '£',
+		'lsl' => 'L',
+		'lrd' => '$',
+		'mop' => 'MOP$',
+		'mkd' => 'ден',
+		'mga' => 'Ar',
+		'mwk' => 'MK',
+		'myr' => 'RM',
+		'mvr' => 'Rf',
+		'mro' => 'UM',
+		'mur' => '₨',
+		'mxn' => '$',
+		'mdl' => 'L',
+		'mnt' => '₮',
+		'mad' => '.د.م',
+		'mzn' => 'MT',
+		'nad' => '$',
+		'npr' => '₨',
+		'ang' => 'ƒ',
+		'twd' => 'NT$',
+		'nzd' => '$',
+		'nio' => 'C$',
+		'ngn' => '₦',
+		'nok' => 'kr',
+		'pkr' => '₨',
+		'pab' => 'B/.',
+		'pgk' => 'K',
+		'pyg' => 'Gs',
+		'pen' => 'S/.',
+		'php' => '₱',
+		'pln' => 'zł',
+		'qar' => '﷼',
+		'ron' => 'lei',
+		'rub' => '₽',
+		'rwf' => 'FRw',
+		'shp' => '£',
+		'svc' => '$',
+		'wst' => 'WS$',
+		'std' => 'Db',
+		'sar' => '﷼',
+		'rsd' => 'Дин.',
+		'scr' => '₨',
+		'sll' => 'Le',
+		'sgd' => '$',
+		'sbd' => '$',
+		'sos' => 'S',
+		'zar' => 'R',
+		'krw' => '₩',
+		'lkr' => '₨',
+		'srd' => '$',
+		'szl' => 'L',
+		'sek' => 'kr',
+		'chf' => 'Fr.',
+		'tjs' => '',
+		'tzs' => 'TSh',
+		'thb' => '฿',
+		'top' => 'T$',
+		'ttd' => 'TT$',
+		'try' => '₺',
+		'ugx' => 'USh',
+		'uah' => '₴',
+		'aed' => 'د.إ',
+		'usd' => '$',
+		'uyu' => '$U',
+		'uzs' => 'лв',
+		'vuv' => 'Vt.',
+		'vnd' => '₫',
+		'xof' => 'CFA',
+		'yer' => '﷼',
+		'zmw' => 'ZK'
 		];
 
-		if (array_key_exists($code,$symbols))
+		if (array_key_exists($code, $symbols)) {
 			return $symbols[$code];
+		}
 		return '<?>';
+	}
+
+	/**
+	GetReportAmount:
+	@units: the amount to process, in 'indivisible' units as used internally by Stripe e.g. cents
+	@format: currency format template e.g. 'S.00'
+	@symbol: currency symbol string
+	Returns: sans-currency formatted string representing amount e.g. 19.99
+	*/
+	public static function GetReportAmount($units, $format, $symbol)
+	{
+		if (preg_match('/^(.*)?(S)(\W+)?(\d*)$/', $format, $matches)) {
+			$places = strlen($matches[4]);
+			$times = pow(10, $places);
+			$num = number_format($units / $times, $places);
+			if ($matches[1]) {
+				if (strpos('.', $num) !== FALSE) {
+					$num = str_replace('.', $symbol, $num); //workaround PHP<5.4
+				}
+			} else {
+				if ($matches[3] != '.') {
+					$num = str_replace('.', $matches[3], $num);
+				}
+			}
+			return $num;
+		} else {
+			return number_format($units / 100, 2);
+		}
 	}
 
 	/**
@@ -548,23 +581,26 @@ EOS;
 	*/
 	public static function GetPublicAmount($units, $format, $symbol)
 	{
-		if (preg_match('/^(.*)?(S)(\W+)?(\d*)$/',$format,$matches)) {
+		if (preg_match('/^(.*)?(S)(\W+)?(\d*)$/', $format, $matches)) {
 			$places = strlen($matches[4]);
-			$times = pow(10,$places);
-			$num = number_format($units/$times,$places);
+			$times = pow(10, $places);
+			$num = number_format($units / $times, $places);
 			if ($matches[1]) {
-				if (strpos('.',$num) !== FALSE)
-					$num = str_replace('.',$symbol,$num); //workaround PHP<5.4
-				else
+				if (strpos('.', $num) !== FALSE) {
+					$num = str_replace('.', $symbol, $num); //workaround PHP<5.4
+				} else {
 					$num .= $symbol;
+				}
 			} else {
-				if ($matches[3] != '.')
-					$num = str_replace('.',$matches[3],$num);
+				if ($matches[3] != '.') {
+					$num = str_replace('.', $matches[3], $num);
+				}
 				$num = $symbol.$num;
 			}
 			return $num;
-		} else
-			return $symbol.number_format($units/100,2);
+		} else {
+			return $symbol.number_format($units / 100, 2);
+		}
 	}
 
 	/**
@@ -576,8 +612,8 @@ EOS;
 	*/
 	public static function CleanPublicAmount($units, $format, $symbol)
 	{
-		$a = self::GetPrivateAmount($units,$format,$symbol);
-		return self::GetPublicAmount($a,$format,$symbol);
+		$a = self::GetPrivateAmount($units, $format, $symbol);
+		return self::GetPublicAmount($a, $format, $symbol);
 	}
 
 	/**
@@ -589,15 +625,17 @@ EOS;
 	*/
 	public static function GetPrivateAmount($amount, $format, $symbol)
 	{
-		if (preg_match('/^(.*)?(S)(\W+)?(\d*)$/',$format,$matches)) {
-			if ($matches[1])
-				$num = str_replace($symbol,'.',$amount);
-			else
-				$num = str_replace([$symbol,$matches[3]],['','.'],$amount);
+		if (preg_match('/^(.*)?(S)(\W+)?(\d*)$/', $format, $matches)) {
+			if ($matches[1]) {
+				$num = str_replace($symbol, '.', $amount);
+			} else {
+				$num = str_replace([$symbol, $matches[3]], ['', '.'], $amount);
+			}
 			$places = strlen($matches[4]);
-			$times = pow(10,$places);
+			$times = pow(10, $places);
 			return (int)($num * $times);
-		} else
-			return preg_replace('/\D/','',$amount) + 0; //assume 'raw' is good enough, in this context
+		} else {
+			return preg_replace('/\D/', '', $amount) + 0; //assume 'raw' is good enough, in this context
+		}
 	}
 }
