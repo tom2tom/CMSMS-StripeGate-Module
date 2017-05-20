688466d3cd3b410580e0208052487853c36f0e09
tom2tom <tpgww@onepost.net>
2017-05-20 19:52:47 +1000

    cleanups

--- action.administer.php
+++ action.administer.php
@@ -1,55 +1,35 @@
 <?php
 #----------------------------------------------------------------------
 # This file is part of CMS Made Simple module: StripeGate
-# Copyright (C) 2016 Tom Phane <tpgww@onepost.net>
+# Copyright (C) 2016-2017 Tom Phane <tpgww@onepost.net>
 # Refer to licence and other details at the top of file StripeGate.module.php
 # More info at http://dev.cmsmadesimple.org/projects/stripegate
 #----------------------------------------------------------------------
-
-if (!function_exists('get_report_amount')) {
- function get_report_amount($units,$format,$char)
- {
-	if (preg_match('/^(.*)?(S)(\W+)?(\d*)$/',$format,$matches)) {
-		$places = strlen($matches[4]);
-		$times = pow(10,$places);
-		$num = number_format($units/$times,$places);
-		if ($matches[1]) {
-			if (strpos('.',$num) !== FALSE)
-				$num = str_replace('.',$char,$num); //workaround PHP<5.4
-		} else {
-			if ($matches[3] != '.')
-				$num = str_replace('.',$matches[3],$num);
-		}
-		return $num;
-	} else
-		return number_format($units/100,2);
- }
-}
 
 $padm = $this->CheckPermission('ModifyStripeGateProperties');
 $pmod = $padm || $this->CheckPermission('ModifyStripeAccount');
 $psee = $padm || $this->CheckPermission('UseStripeAccount');
 
-if (!($padm || $pmod || $psee)) exit;
+if (!($padm || $pmod || $psee)) {
+	exit;
+}
 
 $aid = (int)$params['account_id'];
+
 $pref = cms_db_prefix();
-$row = $db->GetRow('SELECT name,currency,amountformat FROM '.$pref.'module_sgt_account WHERE account_id=?',[$aid]);
-$symbol = StripeGate\Utils::GetSymbol($row['currency']);
+$row = $db->GetRow('SELECT name,currency,amountformat FROM '.$pref.'module_sgt_account WHERE account_id=?', [$aid]);
 
 $tplvars = [
 	'pmod' => $pmod,
-	'backtomod_nav' => $this->CreateLink($id,'defaultadmin',$returnid,'&#171; '.$this->Lang('title_mainpage')),
-	'start_form' => $this->CreateFormStart($id,'processrecords',$returnid,'POST','','','',['account_id'=>$aid]),
+	'backtomod_nav' => $this->CreateLink($id, 'defaultadmin', $returnid, '&#171; '.$this->Lang('title_mainpage')),
+	'start_form' => $this->CreateFormStart($id, 'processrecords', $returnid, 'POST', '', '', '', ['account_id' => $aid]),
 	'end_form' => $this->CreateFormEnd(),
-
-	'title' => $this->Lang('title_account',$row['name']),
-	'title_submitted' => $this->Lang('title_when'),
-	'title_amount' => $this->Lang('title_amount'),
-	'title_what' => $this->Lang('title_what'),
-	'title_for' => $this->Lang('title_who'),
-	'title_token' => $this->Lang('title_token'),
+	'title' => $this->Lang('title_account', $row['name'])
 ];
+
+if (!empty($params['message'])) {
+	$tplvars['message'] = $params['message'];
+}
 
 //script accumulators
 $jsfuncs = [];
@@ -57,33 +37,31 @@ $jsincs = [];
 $jsloads = [];
 $baseurl = $this->GetModuleURLPath();
 
-if (!empty($params['message']))
-	$tplvars['message'] = $params['message'];
-
-$sql = 'SELECT * FROM '.$pref.'module_sgt_record WHERE account_id=? ORDER BY recorded DESC, payfor ASC';
-$data = $db->GetArray($sql,[$aid]);
-
-$theme = ($this->before20) ? cmsms()->get_variable('admintheme'):
+$theme = ($this->before20) ? cmsms()->get_variable('admintheme') :
 	cms_utils::get_theme_object();
+$icon_delete = $theme->DisplayImage('icons/system/delete.gif', $this->Lang('delete'), '', '', 'systemicon');
+$icon_export = $theme->DisplayImage('icons/system/export.gif', $this->Lang('export'), '', '', 'systemicon');
 
-$icon_delete = $theme->DisplayImage('icons/system/delete.gif',$this->Lang('delete'),'','','systemicon');
-$icon_export = $theme->DisplayImage('icons/system/export.gif',$this->Lang('export'),'','','systemicon');
+$symbol = StripeGate\Utils::GetSymbol($row['currency']);
+$data = $db->GetArray('SELECT * FROM '.$pref.'module_sgt_record WHERE account_id=? ORDER BY recorded DESC, payfor ASC', [$aid]);
+$rows = [];
 
 foreach ($data as &$one) {
-	$rid = (int)($one['record_id']);
+	$rid = (int)$one['record_id'];
 	$oneset = new stdClass();
-	$oneset->submitted =  date('Y-m-d H:i:s',$one['recorded']);
-	$oneset->amount = get_report_amount($one['amount'],$row['amountformat'],$symbol);
+	$oneset->submitted =  date('Y-m-d H:i:s', $one['recorded']);
+	$oneset->amount = StripeGate\Utils::GetReportAmount($one['amount'], $row['amountformat'], $symbol);
 	$oneset->what = $one['paywhat'];
 	$oneset->who = $one['payfor'];
 	$oneset->token = $one['identifier'];
-	$oneset->export = $this->CreateLink($id,'exportrecord','',
-		$icon_export,['record_id'=>$rid,'account_id'=>$aid]);
-	if ($pmod)
-	  $oneset->delete = $this->CreateLink($id,'deleterecord','',
-		$icon_delete,['record_id'=>$rid,'account_id'=>$aid],
-		$this->Lang('delitm_confirm',$oneset->token));
-	$oneset->selected = $this->CreateInputCheckbox($id,'sel[]',$rid,-1);
+	$oneset->export = $this->CreateLink($id, 'exportrecord', '',
+		$icon_export, ['record_id' => $rid, 'account_id' => $aid]);
+	if ($pmod) {
+		$oneset->delete = $this->CreateLink($id, 'deleterecord', '',
+		$icon_delete, ['record_id' => $rid, 'account_id' => $aid],
+		$this->Lang('delitm_confirm', $oneset->token));
+	}
+	$oneset->selected = $this->CreateInputCheckbox($id, 'sel[]', $rid, -1);
 	$rows[] = $oneset;
 }
 unset($one);
@@ -93,6 +71,14 @@ $pagerows = 15; //arbitrary initial page-length
 $tplvars['rows'] = $rows;
 $rcount = count($rows);
 if ($rcount) {
+	$tplvars += [
+	'title_submitted' => $this->Lang('title_when'),
+	'title_amount' => $this->Lang('title_amount'),
+	'title_what' => $this->Lang('title_what'),
+	'title_for' => $this->Lang('title_who'),
+	'title_token' => $this->Lang('title_token')
+	];
+
 	if ($rcount > 1) {
 		$jsincs[] = <<<EOS
 <script type="text/javascript" src="{$baseurl}/lib/js/jquery.metadata.min.js"></script>
@@ -112,52 +98,34 @@ EOS;
   currentid: 'cpage',
   countid: 'tpage'
  });
-
 EOS;
-
 		$tplvars['header_checkbox'] =
-			$this->CreateInputCheckbox($id,'selectall',true,false,'onclick="select_all(this);"');
+			$this->CreateInputCheckbox($id, 'selectall', true, false, 'onclick="select_all(this);"');
 
 		$jsfuncs[] = <<<EOS
 function select_all(cb) {
  $('#itemdata > tbody').find('input[type="checkbox"]').attr('checked',cb.checked);
 }
-
 EOS;
-
-/*	$jsfuncs[] = <<< EOS
- $.SSsort.addParser({
-  id: 'textinput',
-  is: function(s,node) {
-   var n = node.childNodes[0];
-   return (n && n.nodeName.toLowerCase() == 'input' && n.type.toLowerCase() == 'text');
-  },
-  format: function(s,node) {
-   return $.trim(node.childNodes[0].value);
-  },
-  watch: true,
-  type: 'text'
- });
-
-EOS;
-*/
-	} else { //$rcount == 0
+	} else { //$rcount == 1
 		$tplvars['header_checkbox'] = NULL;
 	}
 
-	if ($pagerows && $rcount>$pagerows) {
+	if ($pagerows && $rcount > $pagerows) {
 		//more setup for SSsort
 		$choices = [strval($pagerows) => $pagerows];
 		$f = ($pagerows < 4) ? 5 : 2;
 		$n = $pagerows * $f;
-		if ($n < $rcount)
+		if ($n < $rcount) {
 			$choices[strval($n)] = $n;
-		$n *= 2;
-		if ($n < $rcount)
+		}
+		$n += $n;
+		if ($n < $rcount) {
 			$choices[strval($n)] = $n;
+		}
 		$choices[$this->Lang('all')] = 0;
-		$curpg='<span id="cpage">1</span>';
-		$totpg='<span id="tpage">'.ceil($rcount/$pagerows).'</span>';
+		$curpg = '<span id="cpage">1</span>';
+		$totpg = '<span id="tpage">'.ceil($rcount / $pagerows).'</span>';
 
 		$tplvars += [
 			'hasnav' => 1,
@@ -165,9 +133,9 @@ EOS;
 			'prev' => '<a href="javascript:pageback()">'.$this->Lang('previous').'</a>',
 			'next' => '<a href="javascript:pageforw()">'.$this->Lang('next').'</a>',
 			'last' => '<a href="javascript:pagelast()">'.$this->Lang('last').'</a>',
-			'pageof' => $this->Lang('pageof',$curpg,$totpg),
-			'rowchanger' => $this->CreateInputDropdown($id,'pagerows',$choices,-1,$pagerows,
-			'onchange="pagerows(this);"').'&nbsp;&nbsp;'.$this->Lang('pagerows')
+			'pageof' => $this->Lang('pageof', $curpg, $totpg),
+			'rowchanger' => $this->CreateInputDropdown($id, 'pagerows', $choices, -1, $pagerows,
+				'onchange="pagerows(this);"').'&nbsp;&nbsp;'.$this->Lang('pagerows')
 		];
 
 		$jsfuncs[] = <<<EOS
@@ -186,7 +154,6 @@ function pageback() {
 function pagerows(cb) {
  $.SSsort.setCurrent($('#itemdata')[0],'pagesize',parseInt(cb.value));
 }
-
 EOS;
 	} else {
 		$tplvars['hasnav'] = 0;
@@ -207,15 +174,33 @@ function confirm_selected(msg) {
   return false;
  }
 }
-
+function maybe_selected() {
+ if ($('#{$id}duration').val() > 0) {
+  return true;
+ }
+ return (sel_count() > 0);
+}
 EOS;
 	//if ($X) ? perm
-		$tplvars['export'] = $this->CreateInputSubmit($id,'export',$this->Lang('export'),
+		$tplvars['transfers'] = $this->CreateInputSubmit($id, 'transfers', $this->Lang('transfers'),
+			'title="'.$this->Lang('tip_transfers').'" onclick="return maybe_selected();"');
+	$s = $this->Lang('dayscount');
+	$choices = [];
+	foreach ([15, 45, 60] as $n) {
+		$l = sprintf($s, $n);
+		$choices[$l] = $n;
+	}
+	$s = $this->Lang('selected');
+	$choices[$s] = 0;
+	$tplvars['duration'] = $this->CreateInputDropdown($id, 'duration', $choices, -1, $this->GetPreference('transfer_days'),
+			'id="'.$id.'duration"');
+	$tplvars['export'] = $this->CreateInputSubmit($id, 'export', $this->Lang('export'),
 		'title="'.$this->Lang('tip_exportsel').'" onclick="return any_selected();"');
-	if ($pmod)
-		$tplvars['delete'] = $this->CreateInputSubmit($id,'delete',$this->Lang('delete'),
+	if ($pmod) {
+		$tplvars['delete'] = $this->CreateInputSubmit($id, 'delete', $this->Lang('delete'),
 		'title="'.$this->Lang('tip_deletesel2').
 		'" onclick="return confirm_selected(\''.$this->Lang('delsel_confirm2').'\');"');
+	}
 } //$rcount
 /*should never happen, in this context
 else {
@@ -224,12 +209,12 @@ else {
 */
 
 $jsall = NULL;
-StripeGate\Utils::MergeJS($jsincs,$jsfuncs,$jsloads,$jsall);
+StripeGate\Utils::MergeJS($jsincs, $jsfuncs, $jsloads, $jsall);
 unset($jsincs);
 unset($jsfuncs);
 unset($jsloads);
 
-echo StripeGate\Utils::ProcessTemplate($this,'administer.tpl',$tplvars);
+echo StripeGate\Utils::ProcessTemplate($this, 'administer.tpl', $tplvars);
 if ($jsall) {
 	echo $jsall;
 }
