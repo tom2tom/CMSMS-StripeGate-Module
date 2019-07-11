f5e158ca4a26c5e850aa75b653826bf2f808f2b8
tomph <tpgww@onepost.net>
2019-07-11 12:32:24 +1000

    omit redundant constuctor method

--- lib/class.CryptInit.php
+++ lib/class.CryptInit.php
@@ -1,7 +1,7 @@
 <?php
 #----------------------------------------------------------------------
 # This file is part of CMS Made Simple module: StripeGate
-# Copyright (C) 2016-2017 Tom Phane <tpgww@onepost.net>
+# Copyright (C) 2016-2019 Tom Phane <tpgww@onepost.net>
 # Refer to licence and other details at the top of file StripeGate.module.php
 # More info at http://dev.cmsmadesimple.org/projects/stripegate
 #----------------------------------------------------------------------
@@ -9,32 +9,30 @@ namespace StripeGate;
 
 class CryptInit Extends Crypter
 {
-	/**
-	constructor:
-	@mod: reference to current module object
-	@method: optional openssl cipher type to use, default 'BF-CBC'
-	@stretches: optional number of extension-rounds to apply, default 8192
+	/*
+	PARENT::constructor:
+	@param $mod reference to current module object
+	@param string $method: optional openssl cipher type to use, default 'BF-CBC'
+	@param int $stretches: optional number of extension-rounds to apply, default 8192
 	*/
-	public function __construct(&$mod, $method='BF-CBC', $stretches=parent::STRETCHES)
+/*	public function __construct(&$mod, $method='BF-CBC', $stretches=parent::STRETCHES)
 	{
 		parent::__construct($mod, $method, $stretches);
 	}
-
+*/
 	/**
 	init_crypt:
 	Must be called ONCE (during installation and/or after any localisation change)
 	before any hash or preference-crypt
-	@s: optional 'localisation' string, default ''
+	@param string $s: optional 'localisation' string, default ''
 	*/
-	final public function init_crypt($s='')
+	final public function init_crypt($s = '')
 	{
 		if (!$s) {
-			$s = cmsms()->GetConfig()['ssl_url'].$this->mod->GetModulePath().parent::SKEY;
+			$s = cmsms()->GetConfig()['root_url'].$this->mod->GetModulePath().parent::SKEY;
 		}
-		$value = str_shuffle(openssl_random_pseudo_bytes(9).microtime(TRUE));
-		$value = $this->encrypt($value,
-			hash_hmac('sha256', $s, parent::SKEY));
-		$this->mod->SetPreference(hash('tiger192,3', $s),
-			base64_encode($value));
+		$t = str_shuffle(openssl_random_pseudo_bytes(9).microtime(TRUE));
+		$value = $this->encrypt($t, hash_hmac('sha256', $s, parent::SKEY));
+		$this->mod->SetPreference(hash('tiger192,3', $s), base64_encode($value));
 	}
 }
